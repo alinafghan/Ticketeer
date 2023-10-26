4035291a3ec9b47757b0030f97c7a1f89dc6a4ba
Alina <alinanasirafghan@gmail.com>
2023-10-26 23:33:38 +0500

    fixing userController

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,16 +1,11 @@
-
-const {getConnection} = require("../connection");
-
-
+const {getConnection} = require('../connection');
 
 module.exports = {
-
-  
-    removeAllDeserts: async function (req, res){
+    removeAllusers: async function (req, res){
         let connection ;
         try {
             connection = await getConnection();
-            const query = "TRUNCATE TABLE Deserts";
+            const query = "TRUNCATE TABLE users";
             const options={
                 autoCommit: true, // Commit each insert immediately
             }
@@ -31,33 +26,30 @@ module.exports = {
             }
         }
     },
-
-    populateDeserts: async function (req, res){
+    populateusers: async function (req, res){
 
         let connection ;
         try {
-            connection = await getConnection();
-            const data = [
-                ['Pecan Pie', 3.0, 40.0, 20.0, 4.79],
-                ['Tiramisu', 3.8, 25.0, 12.0, 5.99],
-                ['Cupcake', 1.8, 22.0, 8.0, 2.49],
-                ['Brownie', 2.5, 30.0, 12.0, 3.29],
-                ['Lemon Tart', 2.3, 20.5, 10.8, 4.79],
-                ['Strawberry Shortcake', 3.5, 29.0, 14.2, 5.49],
-                ['Chocolate Mousse', 4.0, 28.5, 15.7, 6.99],
-                ['Peach Cobbler', 2.7, 34.0, 11.3, 4.29],
-                ['Banana Split', 3.2, 45.0, 18.5, 7.99],
-                ['IceCream Sandwich', 3.2, 45.0, 18.5, 7.99],
-              ];
+
             
-              for (const dessert of data) {
-                const query = `INSERT INTO Deserts (deserts, protein, carbs, fat, unit_price) VALUES (:1,:2,:3,:4,:5)`;
-                const binds = dessert; // Bind the dessert array directly
-                const options = {
-                    autoCommit: true, // Commit each insert immediately
+            connection = await getConnection();
+            const datausers = [
+                [1,'Beyonce','beyonce@gmail.com',3028898756,'beybey2828','Colorado', 1],
+                [2,'TSwizzle', 'taylorswift@gmail.com',3028898756,'password1','Colorado',1],
+                [3,'Jayz','jayzbayz@gmail.com',3028898756,'password1', 'Colorado',1],
+                [2,'Spongebob Squarepants','squidwardfan@gmail.com',3028898756,'password1','Colorado', 3],
+                [4,'the Strokes','roomonfire@hotmail.com',3028898756, 'password1','Colorado',1],
+                [5,'Nasty Nas','theking@gmail.com',3028898756,'password1','Colorado', 1],
+            ];
+            
+            for (const usersData of datausers) {
+                const queryusers = `INSERT INTO users (user_id, username, email, phone_number,password, city_state_country, user_type) VALUES (:1, :2, :3)`;
+                const bindsusers = usersData; // Bind the usersData array directly
+                const optionsusers = {
+                  autoCommit: true, // Commit each insert immediately
                 };
                 // console.log(query , "aaa----------->>>>")
-                await connection.execute(query,binds,options);
+                await connection.execute(queryusers,bindsusers,optionsusers);
               }
 
               res.status(202).send("Populated");
@@ -79,13 +71,11 @@ module.exports = {
         }
     },
 
-
     GetWholeTable: async function  (req, res){
         let connection ;
         try {
-          console.log("hitttt--<<<<<<")
             connection = await getConnection();
-            const table = await connection.execute("SELECT * FROM Deserts");
+            const table = await connection.execute("select * from users");
             // console.log(table.rows);
             res.status(200).send(table);
           } catch (error) {
@@ -104,15 +94,13 @@ module.exports = {
         // return table;
     },
 
-    getDesertwithCondition: async function (req, res){
+    getuserswithCondition: async function (req, res){
         let connection ;
         try {
-            // console.log(req, "req from getDesertwithCondition")
-            connection = await getConnection();
-            const query = `SELECT * FROM Deserts WHERE ${req.body.condition}`;
             
-            // bind = [req.body.condition];
-            // console.log(bind[0], "bind")
+            connection = await getConnection();
+            const query = `SELECT users.*,users.user_id, users.username,users.email, users.phone_number,users.password, users.city_state_country, users.user_type FROM users WHERE ${req.body.condition}`;
+          
             const table = await connection.execute(query);
             // console.log(table.rows);
             res.status(200).send(table);
@@ -131,108 +119,148 @@ module.exports = {
         } 
     },
 
-    AddNewDesert : async function (req, res){
-      let connection ;
-      try {
-        connection = await getConnection();
-        
-        const bind = [req.body.deserts, req.body.protein, req.body.carbs, req.body.fat, req.body.unit_price]
-
-        console.log(bind, "bind")
-        // INSERT INTO Deserts (deserts, protein, carbs, fat, unit_price)
-        const query = `INSERT INTO Deserts (deserts, protein, carbs, fat, unit_price) VALUES (:1,:2,:3,:4,:5)`;
-        const options = {
-          autoCommit: true, // Commit each insert immediately
-        };
-        await connection.execute(query,bind,options);
-        res.status(202).send("Added");
-      }
-      catch(error){
-        console.log("Error executing SQL query:" ,error)
-        res.status(500).send('Internal Server Error');
-      }
-      finally{
-        if (connection) {
-          try {
-            // Release the connection when done
-            await connection.close();
-          } catch (error) {
-            console.error('Error closing database connection:', error);
-          }
+    AddNewuser: async function (req, res){
+        let connection ;
+        try {
+            connection = await getConnection();
+            const query = `INSERT INTO users (user_id, username,email, phone_number,password, city_state_country, user_type) VALUES (:1, :2, :3, :4)`;
+            const binds = [req.body.user_id, req.body.username,req.body.email, req.body.phone_number,req.body.password, req.body.city_state_country, req.body.user_type];
+            const options = {
+              autoCommit: true, 
+            };
+            
+            await connection.execute(query,binds,options);
+            res.status(202).send("Added");
+        } 
+        catch (error) {
+            console.error('Error executing SQL query:', error);
+            res.status(500).send('Internal Server Error');
+          
+        } 
+        finally {
+            if (connection) {
+              try {
+                // Release the connection when done
+                await connection.close();
+              } catch (error) {
+                console.error('Error closing database connection:', error);
+              }
+            }
         }
-      }
     },
 
-    UpdateDesert: async function (req, res) {
-      let connection;
-      try {
-        connection = await getConnection();
-        const binds = [
-          req.body.deserts,
-          req.body.protein,
-          req.body.carbs,
-          req.body.fat,
-          req.body.unit_price,
-          req.body.desert_id,
-        ];
+    Updateusers: async function (req, res) {
+        let connection;
+        try {
+          connection = await getConnection();
+          const binds = [
+            req.body.user_id,
+            req.body.username,
+            req.body.email, 
+            req.body.phone_number,
+            req.body.password,
+             req.body.city_state_country,
+            req.body.user_type,
+          ];
+      
+          console.log("binds -> ", binds);
+          const query = `UPDATE users SET user_id = :1, username= :2,email =:3, phone_number =: 4,password =: 5, city_state_country =:6, user_type = :7 WHERE ${req.body.condition}`;
+          const options = {
+            autoCommit: true, // Commit each insert immediately
+          }
     
-        console.log("binds -> ", binds);
-        const query = `UPDATE Deserts SET deserts = :1, protein = :2, carbs = :3, fat = :4, unit_price = :5 WHERE desert_id = :6`;
-        const options = {
-          autoCommit: true, // Commit each insert immediately
+          const respnse = await connection.execute(query, binds, options);
+  
+      
+          res.status(202).send("Updated");
+        } catch (error) {
+          console.error("Error executing SQL query:", error);
+          res.status(500).send('Internal Server Error');
+        } finally {
+          if (connection) {
+            try {
+              // Release the connection when done
+              await connection.close();
+            } catch (error) {
+              console.error('Error closing database connection:', error);
+            }
+          }
+        }
+      },
+  
+  
+      DeleteVenueAtID : async function (req, res){
+  
+        let connection ;
+        try{
+          connection = await getConnection();
+          const query = `Delete from users WHERE user_id = :1`;
+          const binds = [req.body.user_id];
+          const options = {
+            autoCommit: true, // Commit each insert immediately
+          };
+  
+          await connection.execute(query,binds,options);
+          res.status(202).send("Deleted");
+        }
+        catch(error){
+          console.log("Error executing SQL query:" ,error)
+          res.status(500).send('Internal Server Error');
+        }
+        finally{
+          if(connection){
+            try{
+              await connection.close();
+            }
+            catch(error){
+              console.log("Error closing database connection:", error);
+            }
+          }
+  
         }
   
-        const respnse = await connection.execute(query, binds, options);
+      },
 
-    
-        res.status(202).send("Updated");
-      } catch (error) {
-        console.error("Error executing SQL query:", error);
-        res.status(500).send('Internal Server Error');
-      } finally {
-        if (connection) {
-          try {
-            // Release the connection when done
-            await connection.close();
-          } catch (error) {
-            console.error('Error closing database connection:', error);
-          }
-        }
-      }
-    },
-
-
-    DeleteDesertAtID : async function (req, res){
-
-      let connection ;
-      try{
-        connection = await getConnection();
-        const query = `BEGIN \n DeleteDesertAndOrders(:1); \n END;`;
-        const binds = [req.body.desert_id];
-        const options = {
-          autoCommit: true, // Commit each insert immediately
-        };
-
-        await connection.execute(query,binds,options);
-        res.status(202).send("Deleted");
-      }
-      catch(error){
-        console.log("Error executing SQL query:" ,error)
-        res.status(500).send('Internal Server Error');
-      }
-      finally{
-        if(connection){
-          try{
-            await connection.close();
-          }
-          catch(error){
-            console.log("Error closing database connection:", error);
-          }
+      DeleteuserWithCondition : async function (req,res) {
+        let connection;
+        try{
+          connection = await getConnection();
+          const query = `Delete from users WHERE ${req.body.condition}`;
+          const binds = [
+            req.body.user_id,
+            req.body.username,
+            req.body.email,
+             req.body.phone_number,
+             req.body.password, 
+             req.body.city_state_country,
+            req.body.user_type,
+          ];
+          const options = {
+            autoCommit: true, // Commit each insert immediately
+          };
+  
+          await connection.execute(query,binds,options);
+          res.status(202).send("Deleted");
         }
 
+        catch(error){
+          console.log("Error executing SQL query:" ,error)
+          res.status(500).send('Internal Server Error');
+        }
+        finally{
+          if(connection){
+            try{
+              await connection.close();
+            }
+            catch(error){
+              console.log("Error closing database connection:", error);
+            }
+          }
+  
+        }
       }
 
-    }
-    
+
+
 
 }
