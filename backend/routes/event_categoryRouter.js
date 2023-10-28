4dc919433b0a4e3b7f14fc514c56d47d34b1267c
Alina <alinanasirafghan@gmail.com>
2023-10-28 21:25:48 +0500

    eventcategory

---
+++ backend/routes/event_categoryRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const event_categoryController = require("../controller/event_categoryController.js");
+
+router.delete("/", event_categoryController.removeAllevent_category);
+
+router.post("/populateevent_categorys", event_categoryController.populateevent_category);
+
+router.get("/GetWholeTable", event_categoryController.GetWholeTable);
+
+router.get("/getevent_categorywithCondition", event_categoryController.getevent_categorywithCondition);
+
+router.post("/AddNewevent_category",event_categoryController.AddNewevent_category);
+
+router.put("/Updateevent_category",event_categoryController.Updateevent_category);
+
+router.delete("/Deleteevent_categoryAtID",event_categoryController.Deleteevent_categoryAtID);
+
+router.delete("/Deleteevent_categoryAtID",event_categoryController.Deleteevent_categoryWithCondition);
+
+
+module.exports = router;
