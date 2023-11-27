f6cc39798072bebe34a52eba4f1be1717d20d239
Alina <alinanasirafghan@gmail.com>
2023-11-27 19:48:43 +0500

    homepage grid fix

--- frontend-alina/src/HomePage.jsx
+++ frontend-alina/src/HomePage.jsx
@@ -1,280 +1,230 @@
 import React from 'react';
-import {Typography,Card,CardActions,CardContent,CardMedia,AppBar,Button,TextField,CssBaseline,Grid,Toolbar, Container, CardActionArea} from '@material-ui/core';
-import{Search} from '@material-ui/icons';
+import {
+  Typography,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  AppBar,
+  Button,
+  TextField,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  CardActionArea
+} from '@material-ui/core';
+import { Search } from '@material-ui/icons';
 import useStyles from './styles';
-import Sidebar from './Sidebar'
+import Sidebar from './Sidebar';
 
 const HomePage = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  return (
+    <>
+      <CssBaseline>
+        <AppBar className={classes.Appbar}>
+          <Toolbar style={{ display: 'flex', justifyContent: 'center' }}>
+            <Sidebar></Sidebar>
+            <Typography className={classes.header} variant="h4">Ticketeer</Typography>
+            <div style={{ marginLeft: 'auto' }}>
+              <Search></Search>
+            </div>
+            <TextField
+              label='Search...'
+              variant='outlined'
+              InputProps={{
+                style: {
+                  borderRadius: "30px",
+                  backgroundColor: '#FFFFFF',
+                }
+              }}
+            ></TextField>
+          </Toolbar>
+        </AppBar>
 
-    return(
+        <br></br>
+        <br></br>
+        <br></br>
 
-        <>
+        <main className={classes.main}>
+          <div>
+            <Container style={{ display: 'flex' }}>
+              <Typography variant='h5' color='textPrimary' gutterBottom>
+                Happening soon at a location near you! <a href="/"> Change location</a>
+              </Typography>
+              <Typography className={classes.filter} variant='p'>Filter by: <a href="/"> date</a></Typography>
+            </Container>
 
-        <CssBaseline>
-
-            <AppBar className = {classes.Appbar}>
-
-                <Toolbar style = {{display : 'flex', justifyContent : 'center'}}>
-
-                <Sidebar>
-
-                 </Sidebar>
-
-                <Typography className = {classes.header} variant = "h4">Ticketeer</Typography>
-
-                <div style = {{ marginLeft: 'auto' }}>
-    
-                   <Search ></Search>  
-
-                   </div>
-
-
-                    <TextField label = 'Search...' variant = 'outlined' InputProps={{
-    style: {
-      borderRadius: "30px",
-      backgroundColor: '#FFFFFF',
-    }
-  }}>       </TextField>
-                   
-                    
-                </Toolbar>
-            </AppBar>
-
-            
             <br></br>
-<br></br>
-<br></br>
+            <br></br>
+            <br></br>
+            <br></br>
 
+            <div className={classes.gridContainer}>
+              <Grid container spacing={9} justify='center'>
 
-            <main className = {classes.main}>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
 
-                <div>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
 
-                    <Container style = {{display : 'flex'}}>
-                        <Typography variant = 'h5'  color = 'textPrimary' gutterBottom>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              </Grid>
 
-                            Happening soon at a location near you! <a href = "/"> Change location</a>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
 
-                            </Typography>
+              <Grid container spacing={9} justify='center'>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
 
-                             <Typography className = {classes.filter} variant = 'p'>Filter by: <a href = "/"> date</a></Typography>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
 
-                    </Container>
+                <Grid item xs={12} sm={3}>
+                  <Card sx={{ maxWidth: 340 }}>
+                    <CardActionArea>
+                      <CardMedia
+                        sx={{ height: 180 }}
+                        image="the_strokes.jpg"
+                        title="event_icon"
+                      />
+                      <CardContent>
+                        <CardActions>
+                          <Button size="big">Event Name</Button>
+                          <Button size="big">Venue</Button>
+                          <Button size="big">Date</Button>
+                        </CardActions>
+                        <Typography variant="body" color="text.secondary">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper.
+                          Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna.
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </main>
 
-
-                    <br></br>
-<br></br>
-<br></br>
-<br></br>
-
-
-                    <div>
-                        <Grid container spacing = {9} justify = 'center'>
-
-                            <Grid item>
-
-                        <Card sx={{ maxWidth: 340 }}>
-                            <CardActionArea>
-                            <CardMedia
-                                       sx={{ height: 180 }}
-                                       image="fairytale.jpg"
-                                        title="event_icon"
-                                        />
-                           <CardContent>
-                           <CardActions>
-                         <Button size="big">Event Name</Button>
-                         <Button size="big">Venue</Button>
-                         <Button size="big">Date</Button>
-                            </CardActions>
-                                   {/* <Typography gutterBottom variant="h5" component="div">
-                                               Event Name - Venue - Date
-                                   </Typography> */}
-                                 <Typography variant="body" color="text.secondary">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-                                 Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-                                    </Typography>
-                           </CardContent>
-                           </CardActionArea>
-                           </Card>
-
-                           </Grid>
-
-
-                           <Grid item>
-
-<Card sx={{ maxWidth: 340 }}>
-    <CardActionArea>
-    <CardMedia
-               sx={{ height: 180}}
-               image="the_strokes.jpg"
-                title="event_icon"
-                />
-   <CardContent>
-   <CardActions>
- <Button size="big">Event Name</Button>
- <Button size="big">Venue</Button>
- <Button size="big">Date</Button>
-    </CardActions>
-           {/* <Typography gutterBottom variant="h5" component="div">
-                       Event Name - Venue - Date
-           </Typography> */}
-         <Typography variant="body" color="text.secondary">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-         Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-            </Typography>
-   </CardContent>
-   </CardActionArea>
-   </Card>
-
-   </Grid>
-   <Grid item>
-
-<Card sx={{ maxWidth: 340 }}>
-    <CardActionArea>
-    <CardMedia
-               sx={{ height: 180 }}
-               image="the_strokes.jpg"
-                title="event_icon"
-                />
-   <CardContent>
-   <CardActions>
- <Button size="big">Event Name</Button>
- <Button size="big">Venue</Button>
- <Button size="big">Date</Button>
-    </CardActions>
-           {/* <Typography gutterBottom variant="h5" component="div">
-                       Event Name - Venue - Date
-           </Typography> */}
-         <Typography variant="body" color="text.secondary">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-         Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-            </Typography>
-   </CardContent>
-   </CardActionArea>
-   </Card>
-
-   </Grid>
-
-   </Grid>
-
-
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-
-
-                        <Grid container spacing = {9} justify = 'center'>
-                        <Grid item>
-
-<Card sx={{ maxWidth: 340 }}>
-    <CardActionArea>
-    <CardMedia
-               sx={{ height: 180 }}
-               image="the_strokes.jpg"
-                title="event_icon"
-                />
-   <CardContent>
-   <CardActions>
- <Button size="big">Event Name</Button>
- <Button size="big">Venue</Button>
- <Button size="big">Date</Button>
-    </CardActions>
-           {/* <Typography gutterBottom variant="h5" component="div">
-                       Event Name - Venue - Date
-           </Typography> */}
-         <Typography variant="body" color="text.secondary">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-         Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-            </Typography>
-   </CardContent>
-   </CardActionArea>
-   </Card>
-
-   </Grid> <Grid item>
-
-<Card sx={{ maxWidth: 340 }}>
-    <CardActionArea>
-    <CardMedia
-               sx={{ height: 180 }}
-               image="the_strokes.jpg"
-                title="event_icon"
-                />
-   <CardContent>
-   <CardActions>
- <Button size="big">Event Name</Button>
- <Button size="big">Venue</Button>
- <Button size="big">Date</Button>
-    </CardActions>
-           {/* <Typography gutterBottom variant="h5" component="div">
-                       Event Name - Venue - Date
-           </Typography> */}
-         <Typography variant="body" color="text.secondary">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-         Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-            </Typography>
-   </CardContent>
-   </CardActionArea>
-   </Card>
-
-   </Grid> <Grid item>
-
-<Card sx={{ maxWidth: 340 }}>
-    <CardActionArea>
-    <CardMedia
-               sx={{ height: 180 }}
-               image="the_strokes.jpg"
-                title="event_icon"
-                />
-   <CardContent>
-   <CardActions>
- <Button size="big">Event Name</Button>
- <Button size="big">Venue</Button>
- <Button size="big">Date</Button>
-    </CardActions>
-           {/* <Typography gutterBottom variant="h5" component="div">
-                       Event Name - Venue - Date
-           </Typography> */}
-         <Typography variant="body" color="text.secondary">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales sollicitudin mi eget semper. 
-         Suspendisse eget nisi quis tortor ultrices congue vel vel lorem. Maecenas ac ex urna. 
-            </Typography>
-   </CardContent>
-   </CardActionArea>
-   </Card>
-
-   </Grid>
-                        </Grid>
-
-                       
-
-
-                    </div>
-
-                </div>
-            </main>
-
-
-           <footer align = 'center'>
-
-           <br></br>
-<br></br>
-<br></br>
-<br></br>
-
-
-                <Typography variant = 'h5'>  copyright © Ticketeer 2023 </Typography>
-                <p>Alina Afghan 24491</p>
-
-
-                </footer>
-          
-        </CssBaseline>
-        
-        </>
-    )
+        <footer align='center'>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Typography variant='h5'>  copyright © Ticketeer 2023 </Typography>
+          <p>Alina Afghan 24491</p>
+        </footer>
+      </CssBaseline>
+    </>
+  );
 }
 
 export default HomePage;
--- frontend-alina/src/Ticketeer.jsx
+++ frontend-alina/src/Ticketeer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography,CssBaseline,Button} from '@material-ui/core';
+import {Typography,CssBaseline,Button, Divider} from '@material-ui/core';
 import frontpageStyles from './Ticketeer_styles';
 import FrontSideBar from './FrontSidebar';
 
@@ -13,29 +13,78 @@ const Ticketeer = () => {
 
             <div className={classes.headerDiv}>
 
+                <div className = {classes.FrontSidebar}>
+
                 <FrontSideBar></FrontSideBar>
 
+                </div>
 
-                <Typography variant = 'h1'> TICKETEER </Typography>
 
-                <Typography variant = 'h1'> Buy your tickets now, wherever, whenever </Typography>
+                <Typography className = {classes.logintitle} variant = 'h1'> TICKETEER </Typography>
 
-                
+                <Typography className= {classes.frontpageLine} variant = 'h4'> Buy your tickets now, wherever, whenever </Typography>
 
 
             </div>
+
+            <Divider></Divider>
 
             <div className = {classes.bodyDiv}>
 
                 <div className = {classes.ButtonsDiv}>
 
-                    <Button></Button>
+                    <Button className = {classes.LoginButton} > LOGIN </Button>
 
-                    <Button></Button>
+                    <br></br>
+                    <br></br>
+
+                    <Button className = {classes.RegisterButton}> REGISTER </Button>
 
 
 
                 </div>
+
+
+            </div>
+
+            <div className = {classes.middleDiv}>
+
+                <Typography className = {classes.middleDivWriting}>
+
+                    lorem ispum dolor hbfuerniwrnb dfvev trbnt nbrtkbrg brgb
+                </Typography>
+
+
+
+
+            </div>
+
+            <div className = {classes.middle2Div}>
+
+                <div className = {classes.middle2Left}>
+
+                    <Typography className = {classes.middle2Leftwriting}>leftside</Typography>
+
+                </div>
+
+                <div className = {classes.middle2Mid}>
+
+                    <Typography className = {classes.middle2midwriting}>middle</Typography>
+                    
+                </div>
+
+                <div className = {classes.middle2Right}>
+
+                    <Typography className = {classes.middle2Rightwriting}>rightside</Typography>
+                    
+                </div>
+
+
+
+
+
+
+
 
 
             </div>
