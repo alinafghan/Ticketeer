327da7d8641d25efcd874c2e4be5c91f77e86c8b
Alina <alinanasirafghan@gmail.com>
2023-12-04 23:45:17 +0500

    more on booking page

--- frontend-alina/src/Pages/BookTicketPage.jsx
+++ frontend-alina/src/Pages/BookTicketPage.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from "react";
 import {
+  AppBar,
   CssBaseline,
-  FormControl,
-  OutlinedInput,
-  MenuItem,
-  Select,
-  InputLabel,
   Button,
+  Toolbar,
   TextField,
   Typography,
-  AppBar,
-  Toolbar,
   Card,
   CardContent,
+  Container,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  InputLabel,
+  FormControl,
 } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 import Sidebar from "../Sidebar";
-import CreateEventPage_styles from "../Styling/CreateEventPage_styles";
+import BookTicketPage_styles from "../Styling/BookTicketPage_styles";
 import useStyles from "../Styling/styles";
-import api from "../api";
 import { useNavigate } from "react-router-dom";
+import api from "../api";
 
 const BookTicketPage = () => {
   const Navigate = useNavigate();
@@ -30,16 +31,16 @@ const BookTicketPage = () => {
     event_id: "",
   });
 
-  const handleInputChange = (bookings) => {
+  const handleInputChange = (booking) => {
     setFormData({
       ...formData,
-      [bookings.target.name]: bookings.target.value,
+      [booking.target.name]: booking.target.value,
     });
   };
 
-  const handleEvent = async () => {
+  const handleBookings = async () => {
     try {
-      console.log("Handle Event function called");
+      console.log("Handle Bookings function called");
       console.log("Form Data:", formData);
       const response = await fetch(
         "http://localhost:3005/bookings/addnewBookings",
@@ -71,113 +72,41 @@ const BookTicketPage = () => {
     }
   };
 
-  const classes = CreateEventPage_styles();
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState("");
+
+  useEffect(() => {
+    api
+      .get("/events/GetWholeTable")
+      .then((response) => {
+        console.log("API Data:", response.data);
+        setEvents(
+          response.data.map((event) => ({
+            event_id: event[0],
+            event_name: event[1],
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+      });
+  }, []);
+
+  const handleChangeEvent = (selectedEventId) => {
+    const selectedEventObject = events.find(
+      (event) => event.event_id === selectedEventId
+    );
+    setSelectedEvent(selectedEventObject);
+    console.log("Selected Event:", selectedEventObject);
+    setFormData({
+      ...formData,
+      event_id: selectedEventObject ? selectedEventObject.event_id : "",
+    });
+    console.log("FormData after event Change:", formData);
+  };
+
+  const classes = BookTicketPage_styles();
   const commonclasses = useStyles();
-
-  const [venues, setVenues] = useState([]);
-  const [selectedVenue, setSelectedVenue] = useState("");
-
-  useEffect(() => {
-    api
-      .get("/venues/GetWholeTable")
-      .then((response) => {
-        console.log("API Data:", response.data);
-        setVenues(
-          response.data.map((venue) => ({
-            venue_id: venue[0],
-            venue_name: venue[1],
-          }))
-        );
-      })
-      .catch((error) => {
-        console.error("API Error:", error);
-      });
-  }, []);
-
-  const handleChangeVenue = (selectedVenueId) => {
-    const selectedVenueObject = venues.find(
-      (venue) => venue.venue_id === selectedVenueId
-    );
-    setSelectedVenue(selectedVenueObject);
-    console.log("Selected Venue:", selectedVenueObject);
-    setFormData({
-      ...formData,
-      venue_id: selectedVenueObject ? selectedVenueObject.venue_id : "",
-    });
-    console.log("FormData after venue Change:", formData);
-  };
-
-  const [performers, setPerformers] = useState([]);
-  const [selectedPerformer, setSelectedPerformer] = useState("");
-
-  useEffect(() => {
-    api
-      .get("/performers/GetWholeTable")
-      .then((response) => {
-        console.log("API Data:", response.data);
-        setPerformers(
-          response.data.map((performer) => ({
-            performer_id: performer[0],
-            performer_name: performer[1],
-          }))
-        );
-        console.log("API Data:", response.data);
-      })
-      .catch((error) => {
-        console.error("API Error:", error);
-      });
-  }, []);
-
-  const handleChangePerformer = (selectedPerformerId) => {
-    const selectedPerformerObject = performers.find(
-      (performer) => performer.performer_id === selectedPerformerId
-    );
-    setSelectedPerformer(selectedPerformerObject);
-    console.log("Selected Performer:", selectedPerformerObject);
-    setFormData({
-      ...formData,
-      performer_id: selectedPerformerObject
-        ? selectedPerformerObject.performer_id
-        : "",
-    });
-    console.log("FormData after Performer Change:", formData);
-  };
-
-  const [eventCategories, setEventCategories] = useState([]);
-  const [selectedEventCategory, setSelectedEventCategory] = useState("");
-
-  useEffect(() => {
-    api
-      .get("/event_category/GetWholeTable")
-      .then((response) => {
-        console.log("API Data:", response.data);
-        setEventCategories(
-          response.data.map((event_category) => ({
-            event_category_id: event_category[0],
-            event_category_name: event_category[1],
-          }))
-        );
-      })
-      .catch((error) => {
-        console.error("API Error:", error);
-      });
-  }, []);
-
-  const handleChangeEventCategory = (selectedEventCategoryId) => {
-    const selectedEventCategoryObject = eventCategories.find(
-      (eventCategory) =>
-        eventCategory.event_category_id === selectedEventCategoryId
-    );
-    setSelectedEventCategory(selectedEventCategoryObject);
-    console.log("Selected Event Category:", selectedEventCategoryObject);
-    setFormData({
-      ...formData,
-      event_category_id: selectedEventCategoryObject
-        ? selectedEventCategoryObject.event_category_id
-        : "",
-    });
-    console.log("FormData after Event Category Change:", formData);
-  };
 
   return (
     <CssBaseline>
@@ -205,220 +134,55 @@ const BookTicketPage = () => {
         </AppBar>
       </div>
 
-      <div className={classes.middleDiv}>
-        <div className={classes.ADiv}>
-          <div className={classes.cardContainer}>
-            <Card className={classes.EventCard} sx={{ maxWidth: 500 }}>
-              <CardContent>
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-1"
-                    variant="outlined"
-                    label="Name of Event"
-                    name="event_name"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <FormControl style={{ width: "100%" }}>
-                    <InputLabel
-                      id="demo-simple-select-helper-label"
-                      style={{ textAlign: "center", marginLeft: 15 }}
-                    >
-                      Venue for event
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-helper-label"
-                      id="demo-simple-select-helper"
-                      value={selectedVenue ? selectedVenue.venue_id : ""}
-                      label="Venue"
-                      name="venue_id"
-                      onChange={(event) =>
-                        handleChangeVenue(event.target.value)
-                      }
-                      input={<OutlinedInput label="Name" />}
-                      style={{ width: "100%" }}
-                    >
-                      {venues.map((venue) => (
-                        <MenuItem key={venue.venue_id} value={venue.venue_id}>
-                          {venue.venue_name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-2"
-                    variant="outlined"
-                    label="Date of Event"
-                    name="event_date"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-1"
-                    variant="outlined"
-                    label="Start Time..."
-                    name="start_time"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-1"
-                    variant="outlined"
-                    label="End Time..."
-                    name="end_time"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-
-                <br />
-              </CardContent>
-            </Card>
-
-            <div style={{ width: 16 }} />
-
-            <Card className={classes.EventCard} sx={{ maxWidth: 500 }}>
-              <CardContent>
-                <div className={classes.formField}>
-                  <FormControl style={{ width: "100%" }}>
-                    <InputLabel
-                      id="demo-simple-select-helper-label"
-                      style={{ textAlign: "center", marginLeft: 15 }}
-                    >
-                      Performer for event
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-helper-label"
-                      id="demo-simple-select-helper"
-                      value={
-                        selectedPerformer ? selectedPerformer.performer_id : ""
-                      }
-                      label="Performer"
-                      name="performer_id"
-                      onChange={(event) =>
-                        handleChangePerformer(event.target.value)
-                      }
-                      input={<OutlinedInput label="Name" />}
-                      style={{ width: "100%" }}
-                    >
-                      {performers.map((performer) => (
-                        <MenuItem
-                          key={performer.performer_id}
-                          value={performer.performer_id}
-                        >
-                          {performer.performer_name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <FormControl style={{ width: "100%" }}>
-                    <InputLabel
-                      id="demo-simple-select-helper-label"
-                      style={{ textAlign: "center", marginLeft: 15 }}
-                    >
-                      Category of event
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-helper-label"
-                      id="demo-simple-select-helper"
-                      value={
-                        selectedEventCategory
-                          ? selectedEventCategory.event_category_id
-                          : ""
-                      }
-                      label="Event Category"
-                      name="event_category_id"
-                      onChange={(event) =>
-                        handleChangeEventCategory(event.target.value)
-                      }
-                      input={<OutlinedInput label="Name" />}
-                      style={{ width: "100%" }}
-                    >
-                      {eventCategories.map((eventCategory) => (
-                        <MenuItem
-                          key={eventCategory.event_category_id}
-                          value={eventCategory.event_category_id}
-                        >
-                          {eventCategory.event_category_name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-2"
-                    variant="outlined"
-                    label="Number of Tickets"
-                    name="num_of_tickets"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-3"
-                    variant="outlined"
-                    label="Number of VIP Tickets"
-                    name="num_of_VIP_tickets"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-                <div className={classes.formField}>
-                  <TextField
-                    required
-                    id="outlined-required-4"
-                    variant="outlined"
-                    label="Number of General Tickets"
-                    name="num_of_general_tickets"
-                    className={classes.textField}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <br />
-              </CardContent>
-            </Card>
-          </div>
-        </div>
-
-        <div className={classes.ButtonDiv}>
-          <Button onClick={handleEvent} className={classes.Button}>
-            CONTINUE
-          </Button>
-        </div>
-      </div>
+      <Container className={classes.middleDiv}>
+        <Card className={classes.EventCard} sx={{ maxWidth: 800 }}>
+          <CardContent>
+            <Typography variant="h4" align="center">
+              Ticketeer
+            </Typography>
+            <div className={classes.formField}>
+              <FormControl style={{ width: "100%" }}>
+                <InputLabel
+                  id="demo-simple-select-helper-label"
+                  style={{ textAlign: "center", marginLeft: 15 }}
+                >
+                  choose event
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={selectedEvent ? selectedEvent.event_id : ""}
+                  label="Event"
+                  name="event_id"
+                  onChange={(booking) =>
+                    handleChangeEvent(booking.target.value)
+                  }
+                  input={<OutlinedInput label="Name" />}
+                  style={{ width: "100%" }}
+                >
+                  {events.map((event) => (
+                    <MenuItem key={event.event_id} value={event.event_id}>
+                      {event.event_name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+            <div className={classes.ButtonDiv}>
+              <Button onClick={handleInputChange} className={classes.Button}>
+                CONTINUE
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
 
       <footer align="center">
         <br />
         <br />
         <br />
         <br />
-        <Typography variant="h5"> copyright © Ticketeer 2023 </Typography>
+        <Typography variant="h5">Copyright © Ticketeer 2023</Typography>
         <p>Alina Afghan 24491</p>
       </footer>
     </CssBaseline>
---
+++ frontend-alina/src/Styling/BookTicketPage_styles.js
@@ -0,0 +1,51 @@
+import { makeStyles } from "@material-ui/core";
+
+const BookTicketPage_styles = makeStyles((theme) => ({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+      '"Krona one"',
+    ].join(","),
+  },
+
+  middleDiv: {
+    marginTop: 200,
+    justifyContent: "center",
+    width: "70%",
+    height: "80%",
+
+    // flexDirection: "column",
+    // alignItems: "center",
+  },
+
+  ButtonDiv: {
+    justifyContent: "center",
+    display: "flex",
+  },
+
+  formField: {
+    justifyContent: "center",
+    display: "flex",
+  },
+
+  Button: {
+    marginTop: 40,
+    backgroundColor: "#000000",
+    color: "#FCFBF4",
+    fontFamily: "Krona one",
+    borderRadius: 30,
+    padding: 20,
+    position: "relative",
+  },
+}));
+
+export default BookTicketPage_styles;
